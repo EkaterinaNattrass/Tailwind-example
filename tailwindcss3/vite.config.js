5b60db61b49eef7c62482027f8ddca9550f37fad
EkaterinaNattrass <cattiva2012@gmail.com>
2024-01-21 18:23:07 +0100

    initial commit

---
+++ tailwindcss3/vite.config.js
@@ -0,0 +1,12 @@
+export default {
+    root:'src',
+    publicDir: 'public',
+    outDir: '../docs',
+    optimizeDeps: {
+        entries: [],
+    },
+    server: {
+        open: true, /*open the window whenever the server runs */
+        port: 5173,
+    }
+}   
