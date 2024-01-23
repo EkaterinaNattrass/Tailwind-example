cde28839f40f318cb8645ecf0ae14ff2d25df0d1
EkaterinaNattrass <cattiva2012@gmail.com>
2024-01-23 10:01:53 +0100

    adds different sections and form

--- tailwindcss3/tailwind.config.js
+++ tailwindcss3/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ['./src/index.html', './src/**/*.{vue, js, ts, jsx, tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      'display': ['Lemon', 'serif']
+    }
   },
   plugins: [],
 }
